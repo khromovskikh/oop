24a50974ccb283fc57f4b2b174fbbeca8c31cd08
irishakeks <irishakeks@mail.ru>
2019-02-10 16:58:24 +0700

    Сделала рефакторинг

--- logic_oop.py
+++ logic_oop.py
@@ -1,7 +1,9 @@
 import abc  # библиотека для реализации абстрактных классов
 
 
-class Shape(abc.ABC):  # создаем класс фигур
+class Shape:  # создаем класс фигур
+    shapes_list = []
+
     def __init_(self):
         self.density = 0  # у всех фигур будет плотность
 
@@ -9,37 +11,47 @@ class Shape(abc.ABC):  # создаем класс фигур
     def output_shape(self, output_stream):
         pass
 
+    def input_shape(self, shapes_list, shape_type, shape_params):
+        if int(shape_type) == 1:  # Если 1, то параллелепипед
+            parr = Parallelepiped()  # создаем объект класса
+            parr.input_shape(shape_params, shapes_list)
+        elif int(shape_type) == 2:  # если 2, то шар
+            sphere = Sphere()
+            sphere.input_shape(shape_params, shapes_list)
+        else:
+            print("Ошибка в формате записи данных в файле.")
+
 
 class Parallelepiped(Shape):  # создаем класс параллелепипедов, дочерний классу фигур
-    def __init_(self):
+    def __init_(self,):
         super().__init__(self)  # Вызываем конструктор базового класса, чтобы получить значение плотности
 
-    def input_shape(self, h, w, l, d):  # Присваиваем значения полям класса
-        self.height = h
-        self.width = w
-        self.length = l
-        self.density = d
+    def input_shape(self, line, shapes_list):  # Присваиваем значения полям класса
+        self.h, self.w, self.l, self.d = line  # получаем значения переменных из следующей строки
+        self.d.strip()
+        shapes_list.append(self)
 
     def output_shape(self, output_stream):  # Вывод значений полей
         output_stream.write(": It's parallelepiped: "
-                            "h = " + self.height + ", "
-                            "w = " + self.width + ", "
-                            "l = " + self.length + ", "
-                            "d = " + self.density + "\n")
+                            "h = " + self.h + ", "
+                            "w = " + self.w + ", "
+                            "l = " + self.l + ", "
+                            "d = " + self.d + "\n")
 
 
 class Sphere(Shape):  # Создаем класс шара, дочерный классу фигур
     def __init_(self):
         Shape.__init__(self)  # тоже говорим, что есть значение плотности
 
-    def input_shape(self, r, d):  # Тоже присваиваем значения полям класса
-        self.radius = r
-        self.density = d
+    def input_shape(self, line, shapes_list):  # Тоже присваиваем значения полям класса
+        self.r, self.d = line  # получаем значения переменных из следующей строки
+        self.d.strip()
+        shapes_list.append(self)
 
     def output_shape(self, output_stream):  # Тоже выводим значения полей
         output_stream.write(": It's sphere: "
-                            "r = " + self.radius + ", "
-                            "d = " + self.density + "\n")
+                            "r = " + self.r + ", "
+                            "d = " + self.d + "\n")
 
 
 class Container:
@@ -53,19 +65,9 @@ class Container:
         except OSError:
             return print("Файл с данными не найден.")
 
+        shape = Shape()
         for line in file:
-            if int(line) == 1:  # Если 1, то параллелепипед
-                h, w, l, d = file.readline().split(" ")  # получаем значения переменных из следующей строки
-                tmp_parr = Parallelepiped()  # создаем объект класса
-                tmp_parr.input_shape(h, w, l, d.strip())  # делаем отдельный метод, как в методичке
-                self.shapes_list.append(tmp_parr)  # добавляем в конец списка наш объект
-            elif int(line) == 2:  # если 2, то шар
-                r, d = file.readline().split(" ")
-                tmp_sphere = Sphere()
-                tmp_sphere.input_shape(r, d.strip())
-                self.shapes_list.append(tmp_sphere)
-            else:
-                print("Ошибка в формате записи данных в файле.")
+            shape.input_shape(self.shapes_list, line, file.readline().split(" "))
 
     def output(self, file_name):
         count = 0  # счетччик для номера фигуры
